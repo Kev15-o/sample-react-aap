a049ef8be2b55ca7d46679cd6483895445edd6e4
kev15-o <kevinbijupro@gmail.com>
2026-05-08 15:16:49 +0530

    sampele1

---
+++ my project/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Home from './componente/Home'
+import About from './About'
+import Contact from './Contact'
+import Counter from './componente/Counter'
+
+const App = () => {
+  return (
+    <div>
+      <Home></Home>
+      <About></About>
+      <Contact></Contact>
+      <Counter></Counter>
+    </div>
+  )
+}
+
+export default App
